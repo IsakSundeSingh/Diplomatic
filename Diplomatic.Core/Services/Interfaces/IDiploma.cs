07bc2ec0d4858b019ced9a3bb7eba1b52910085f
Mads Johansen <johansen.max@gmail.com>
2018-06-18 15:53:07 +0200

    Add interfaces for Diploma, DiplomaGenerator and TemplateProvider

---
+++ Diplomatic.Core/Services/Interfaces/IDiploma.cs
@@ -0,0 +1,11 @@
+﻿using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace Diplomatic.Core
+{
+    public interface IDiploma
+    {
+        void Save(string Path);
+    }
+}
--- Diplomatic.Core/Services/Interfaces/ITemplateProvider.cs
+++ Diplomatic.Core/Services/Interfaces/ITemplateProvider.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Diplomatic.Core
 {
-    interface IDiplomaGenerator
+    interface ITemplateProvider
     {
-        IDiploma Generate(Template template);
+        IEnumerable<Template> GetTemplates();
     }
 }
