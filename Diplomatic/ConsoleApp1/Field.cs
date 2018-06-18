ae6a309fe9dea9b668035f2696d2c55c89ffcdce
jonjohansen <jonjohansen2k@gmail.com>
2018-06-18 10:41:33 +0200

    Restructure initial code

---
+++ Diplomatic/ConsoleApp1/Field.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomatic.Core
+{
+    public class Field
+    {
+        public string Name;
+        public int xOffset;
+        public int yOffset;
+        public int Height;
+        public int Width;
+
+        public Field(string name, int x, int y, int h, int w)
+        {
+            Name = name;
+            xOffset = x;
+            yOffset = y;
+            Height = h;
+            Width = w;
+        }
+        public void Deconstruct(out string name, out int x, out int y, out int h, out int w)
+        {
+            name = Name;
+            x = xOffset;
+            y = yOffset;
+            h = Height;
+            w = Width;
+        }
+    }
+}
---
+++ Diplomatic/ConsoleApp1/Template.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomatic.Core
+{
+    [Serializable]
+    public class Template
+    {
+        public string FilePath;
+        public string TemplateName;
+        public IEnumerable<Field> Fields;
+
+        public Template(string path, string name, IEnumerable<Field> fields)
+        {
+            FilePath = path;
+            TemplateName = name;
+            Fields = fields;
+        }
+    }
+
+
+
+}
