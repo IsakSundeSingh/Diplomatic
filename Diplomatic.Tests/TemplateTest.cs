d44902f9fcba84c6419a750165f178c22bb00ce3
Mads Johansen <johansen.max@gmail.com>
2018-06-19 14:47:42 +0200

    Add tests for Templates

--- Diplomatic.Tests/TemplateTest.cs
+++ Diplomatic.Tests/TemplateTest.cs
@@ -7,18 +7,44 @@ namespace Diplomatic.Tests
 {
     public class TemplateTest
     {
-        readonly Template subject;
+        readonly IField[] ValidFields;
+        readonly ITemplateStream ValidStream;
+        Template subject;
 
         public TemplateTest()
         {
-            var mockedStream = new Mock<ITemplateStream>();
-            mockedStream.Setup(IsValid => true);
-            subject = new Template("Test template", mockedStream.Object, new Field[] { });
+            var mockField = new Mock<IField>();
+            mockField.Setup(field => field.IsValid).Returns(true);
+            ValidFields = new IField[] { mockField.Object };
+
+            var mockStream = new Mock<ITemplateStream>();
+            mockStream.Setup(stream => stream.IsValid).Returns(true);
+            ValidStream = mockStream.Object;
+        }
+
+        [Fact]
+        public void ValidatesFieldsAreFilled()
+        {
+            subject = new Template("Template with filled fields", ValidStream, ValidFields);
+            Assert.True(subject.IsValid);
         }
 
         [Fact]
         public void NotValidWithoutFields()
         {
+            Template subject = new Template("Template with no fields", ValidStream, new Field[] { });
+            Assert.False(subject.IsValid);
+        }
+
+        [Fact]
+        public void NotValidWithoutStream()
+        {
+            var mockStream = new Mock<ITemplateStream>();
+            mockStream.Setup(stream => stream.IsValid).Returns(false);
+            var badStream = mockStream.Object;
+
+            Template subject = new Template("Test template", badStream, ValidFields);
+
             Assert.False(subject.IsValid);
         }
     }
