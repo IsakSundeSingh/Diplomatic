d469fe7860074d094b465a4cae595dcfc1759c9d
Mads Johansen <johansen.max@gmail.com>
2018-06-19 13:51:30 +0200

    Add Moq for mocking in tests

---
+++ Diplomatic.Tests/TemplateTest.cs
@@ -0,0 +1,25 @@
+﻿using Diplomatic.Core;
+using Moq;
+using System.IO;
+using Xunit;
+
+namespace Diplomatic.Tests
+{
+    public class TemplateTest
+    {
+        readonly Template subject;
+
+        public TemplateTest()
+        {
+            var mockedStream = new Mock<ITemplateStream>();
+            mockedStream.Setup(IsValid => true);
+            subject = new Template("Test template", mockedStream.Object, new Field[] { });
+        }
+
+        [Fact]
+        public void NotValidWithoutFields()
+        {
+            Assert.False(subject.IsValid);
+        }
+    }
+}
