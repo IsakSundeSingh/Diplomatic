9b4a12987c8fcd552deb87212befb3f8cd5265a7
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2018-06-20 13:06:33 +0200

    Add image resource-locator extension

---
+++ Diplomatic/Extensions/MarkupExtensions/ImageResourceExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Diplomatic.Extensions.Markup
+{
+
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource($"{nameof(Diplomatic)}.Assets.Images.{Source}");
+            return imageSource;
+        }
+    }
+}
