ab8cdbe8d8a417b2401df049d8ff1d90f37d0a8a
Mads Johansen <johansen.max@gmail.com>
2018-06-20 08:48:02 +0200

    Create interface for Fields

---
+++ Diplomatic.Core/Services/Interfaces/IField.cs
@@ -0,0 +1,11 @@
+﻿namespace Diplomatic.Core
+{
+    public interface IField
+    {
+        bool IsValid { get; }
+        string Name { get; }
+        string Value { get; set; }
+
+        void Deconstruct(out int x, out int y, out int w, out int h);
+    }
+}
