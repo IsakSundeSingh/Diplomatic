59123637c47fb76d6c543eaf05e70cd017e0a5bd
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2018-06-20 10:05:24 +0200

    Add some useful converters, behaviors and triggers
    Add entry behavior that changes text color when input text has incorrect format
    Add value-converter that asserts if text matches a pattern or not
    Add value-converter that asserts if a length is positive (e.g. if an entry has input)
    Add a trigger-action that animates a scaling motion to any visual element

---
+++ Diplomatic/Utils/Triggers/ScaleAnimation.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace Diplomatic.Utils.Triggers
+{
+    public class ScaleTriggerAction : TriggerAction<VisualElement>
+    {
+        public int StartsFrom { get; set; } = 0;
+        public uint Duration { get; set; } = 250;
+
+        protected override void Invoke(VisualElement visual)
+        {
+            var animation = new Animation(d =>
+            {
+                double val = StartsFrom == 0 ? d : 1 - d;
+                // Scales the element 5 %
+                visual.Scale = 1.0 + val / 20;
+            });
+
+            visual.Animate("scaling", animation, length: Duration, easing: Easing.SinIn);
+        }
+    }
+}
