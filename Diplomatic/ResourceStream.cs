9ef718eb9c821d9a687a379bcc9e410a29ff6994
jonjohansen <jonjohansen2k@gmail.com>
2018-06-21 12:36:34 +0200

    Rewrite Diplomatic.Core
    Diplomatic.Core is no longer a .NET Framework library, and is transformed
    into a .NET standard library for compability with macOS

---
+++ Diplomatic/ResourceStream.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Diplomatic.Core;
+namespace Diplomatic
+{
+    public class ResourceStream : ITemplateStream
+    {
+        public ResourceStream()
+        {
+        }
+
+        public Stream Stream => new MemoryStream(new byte[] { 0x1, 0x2 }
+                                                );
+
+        public string FilePath => "Halloi";
+
+        public bool IsValid => true;
+    }
+}
