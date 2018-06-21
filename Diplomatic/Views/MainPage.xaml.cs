a9720ef1cafea117b260be1f3f7e7fa8c745450b
Vebjørn Haugland <vebjornhaugland@gmail.com>
2018-06-21 14:59:02 +0200

    Add link to Diplomatic GitHub in main page view.

--- Diplomatic/Views/MainPage.xaml.cs
+++ Diplomatic/Views/MainPage.xaml.cs
@@ -20,7 +20,15 @@ namespace Diplomatic.Views
         void OnShow ( object sender, EventArgs e)
         {
             InfoLabel.IsVisible = !InfoLabel.IsVisible;
+            LinkLabel.IsVisible = !LinkLabel.IsVisible;
         }
+
+        void OnLink ( object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://github.com/Dualog-students/Diplomatic"));
+        }
+
+
 
     }
 }
