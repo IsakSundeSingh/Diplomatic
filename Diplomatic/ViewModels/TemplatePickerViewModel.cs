679412620c960130c3990d4d7598c78794dadbc2
Mads Johansen <johansen.max@gmail.com>
2018-06-22 13:59:02 +0200

    TemplatePickerViewModel scaffold

--- Diplomatic/ViewModels/TemplatePickerViewModel.cs
+++ Diplomatic/ViewModels/TemplatePickerViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+using System;
+using Diplomatic.Utils;
+
 namespace Diplomatic.ViewModels
 {
     public class TemplatePickerViewModel
     {
         public TemplatePickerViewModel()
         {
+            var templates = new ResourceTemplateProvider("templates.json").GetTemplates();
         }
     }
 }
