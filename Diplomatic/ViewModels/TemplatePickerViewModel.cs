7114fe2a507db9f6ce841646325728050ec0eeb4
jonjohansen <jonjohansen2k@gmail.com>
2018-06-22 16:06:58 +0200

    Display templates in Templates View
    - Set TemplateName as property in Template class.
    	Tests support this as functional
    - MainPages NextPage function sets ContextBinding for TemplatePicker view and instantiates the page and ViewModel
    - Template is only viewed with TemplateName as of now

--- Diplomatic/ViewModels/TemplatePickerViewModel.cs
+++ Diplomatic/ViewModels/TemplatePickerViewModel.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Diplomatic.Utils;
+using Diplomatic.Core;
+
 
 namespace Diplomatic.ViewModels
 {
     public class TemplatePickerViewModel
     {
+        public Template[] TemplateList { get; set; }
         public TemplatePickerViewModel()
         {
-            var templates = new ResourceTemplateProvider("templates.json").GetTemplates();
+            TemplateList = new ResourceTemplateProvider("templates.json").GetTemplates().ToArray();
         }
     }
 }
