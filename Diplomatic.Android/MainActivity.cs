7d3779e3266ab5b701e297ed03390c71bc6391cc
Vebjørn Haugland <vebjornhaugland@gmail.com>
2018-06-25 09:03:17 +0200

    Change icon statement of Android solution to be set in AndroidManifest
    Remove icon statement from Android/MainActivity in order to use the icon statement in AndroidManifest in stead. Seems like proper convention, and also resemble the solutions for iOS and UWP projects.

--- Diplomatic.Android/MainActivity.cs
+++ Diplomatic.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Diplomatic.Droid
 {
-    [Activity(Label = "Diplomatic", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Diplomatic", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
