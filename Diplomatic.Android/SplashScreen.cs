9a0fed7e4b5618f746961f389cb4c77fd482b9b9
Vebjørn Haugland <vebjornhaugland@gmail.com>
2018-06-26 13:37:10 +0200

    Add background color matching the dark blue from the Dualog logo

--- Diplomatic.Android/SplashScreen.cs
+++ Diplomatic.Android/SplashScreen.cs
@@ -7,7 +7,7 @@ using Android.Support.V7.App;
 
 namespace Diplomatic.Droid
 {
-    [Activity(Label = "MyApp", MainLauncher = true, NoHistory = true, Theme = "@style/Theme.Splash",
+    [Activity(Label = "Diplomatic", MainLauncher = true, NoHistory = true, Theme = "@style/Theme.Splash",
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class SplashScreen : AppCompatActivity
     {
