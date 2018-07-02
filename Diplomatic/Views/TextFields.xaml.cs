d05e836301eb3e7e14d67ace56728353032aeccc
jonjohansen <jonjohansen2k@gmail.com>
2018-07-02 10:41:06 +0200

    Selected Template object is now passed from text-fields onto Signatures page
    	- This should help with the stored field values in the template object :)

--- Diplomatic/Views/TextFields.xaml.cs
+++ Diplomatic/Views/TextFields.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
-
+using Diplomatic.ViewModels;
+using Diplomatic.Core;
 namespace Diplomatic.Views
 {
     public partial class TextFields : ContentPage
@@ -14,6 +14,8 @@ namespace Diplomatic.Views
         async void NextPage(object sender, EventArgs e)
         {
             var next = new Signatures();
+
+            next.BindingContext = new SignaturePickerViewModel(((TextFieldViewModel)BindingContext).SelectedTemplate);
             // We do not need to submit any info to the page here yet
             await Navigation.PushAsync(next);
         }
