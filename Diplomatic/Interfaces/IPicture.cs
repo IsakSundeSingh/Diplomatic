567c4a572f36ac2a7e3f9a9c793e7201244993e6
jonjohansen <jonjohansen2k@gmail.com>
2018-07-03 15:22:52 +0200

    Saving images!
    Interface for saving images has been added
     - Implemented on iOS
     - Permissions have been set for iOS
    
    Views:
    Result has gotten two buttons
    Save button now has an action, which should run a code-behind function for saving
     NOTE: the name of the image saved in the codebehind should be changed

---
+++ Diplomatic/Interfaces/IPicture.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Diplomatic.Interfaces
+{
+    public interface IPicture
+    {
+        void SavePictureToDisk(string filename, byte[] imageArray);
+    }
+}
--- Diplomatic/Views/Result.xaml.cs
+++ Diplomatic/Views/Result.xaml.cs
@@ -1,20 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Net;
+using Diplomatic.Interfaces;
+using Diplomatic.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Diplomatic.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Result : ContentPage
-	{
-		public Result ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Result : ContentPage
+    {
+        public Result()
+        {
+            InitializeComponent();
+        }
+        public async void SaveButtonClicked(object sender, EventArgs e)
+        {
+            using (var webClient = new WebClient())
+            {
+                byte[] imageBytes = await webClient.DownloadDataTaskAsync(((ResultViewModel)BindingContext).ImageUri);
+                DependencyService.Get<IPicture>().SavePictureToDisk("Navn", imageBytes);
+                await DisplayAlert("Image save", "Your diploma has been saved!", "Ok");
+
+            }
+        }
+    }
 }
