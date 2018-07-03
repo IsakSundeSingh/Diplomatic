14b7bf0036cbe87558f3da457772b1f2b2c6e1fb
jonjohansen <jonjohansen2k@gmail.com>
2018-07-03 15:22:52 +0200

    Saving images!
    Interface for saving images has been added
     - Implemented on iOS
     - Permissions have been set for iOS
    
    Views:
    Result has gotten two buttons
    Save button now has an action, which should run a code-behind function for saving
     NOTE: the name of the image saved in the codebehind should be changed

---
+++ Diplomatic.iOS/Classes/Picture_iOS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Diplomatic.Interfaces;
+using Diplomatic.iOS.Classes;
+using Foundation;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Picture_iOS))]
+namespace Diplomatic.iOS.Classes
+{
+    public class Picture_iOS : IPicture
+    {
+        
+        public void SavePictureToDisk(string filename, byte[] imageArray)
+        {
+            var Img = new UIImage(NSData.FromArray(imageArray));
+            Img.SaveToPhotosAlbum((image, error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine(error);
+                    throw new Exception("Yeeeeeeeeeeeeeeeeeet");
+                }
+            });
+        }
+    }
+}
