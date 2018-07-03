e3483f95545ac240dcea6f6d30be363fb4d4445d
Vebjørn Haugland <vebjornhaugland@gmail.com>
2018-07-03 13:09:19 +0200

    Make cosmetic changes to labels and entries with triggers
    Add triggers to labels (text below logo once logo is clicked) on main page, so the labels are faded in as an animation. Entry fields on entry page view is faded when they are not in focus

---
+++ Diplomatic/Utils/Triggers/OpacityAnimation.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Diplomatic.Utils.Triggers
+{
+    public class OpacityTriggerAction : TriggerAction<VisualElement>
+    {
+        public int StartsFrom { set; get; } = 0;
+        //public double Opacity { get; set; } = 1;
+        //public uint Duration { get; set; } = 10000;
+
+        protected override void Invoke(VisualElement visual)
+        {
+            visual.Animate("label", new Animation((d) => {
+                var val = StartsFrom == 0 ? d : 1 - d;
+                visual.Opacity = val;
+
+            }),
+            length: 500, // milliseconds
+            easing: Easing.Linear);
+        }
+    }
+}
